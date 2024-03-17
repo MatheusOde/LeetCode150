7a340e8ed76d79687705f3a586536209ff34b6f1
MatheusOde <matheusode@gmail.com>
2024-03-17 12:33:41 -0300

    135

---
+++ 135Candy.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def candy(self, ratings):
+        """
+        :type ratings: List[int]
+        :rtype: int
+        """
+        candy_distribution = [1] * len(ratings)
+        i = 0
+        for i in range(len(ratings)):
+            if ratings[i] > ratings[i-1]:
+                candy_distribution[i] += 1
+
+        for i in range(len(ratings)-2, -1, -1):
+            if ratings[i] > ratings[i+1]:
+                candy_distribution[i] += 1
+
+        print(candy_distribution)
+        return sum(candy_distribution)
+
+
+def main():
+    ratings = [1, 0, 2]
+    solution_instance = Solution()
+    print("Solution instance: ", solution_instance.candy(ratings))
+
+
+if __name__ == '__main__':
+    main()
